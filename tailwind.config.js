6aa60d969e0ac5001c8dc1546966a4b51c810e80
Hari Saputra <hari.saputra.dev@gmail.com>
2023-03-21 21:21:18 +0700

    init project, navbar, header

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{html,js}"
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      source: ['Source Sans Pro', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
